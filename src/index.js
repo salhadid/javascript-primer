cbf16d97c5ae594a86be65eb7e808b64872d70c1
smalhadid <smalhadid@gmail.com>
2023-10-05 08:45:00 -0400

    finished primer assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 export function calculateTotal (items, tax) {
-  // TODO
+    let total = 0;
+
+    tax = Math.abs(tax);
+
+    for (const item of items) {
+        total += item.price;
+
+        if (item.taxable) {
+            total += item.price * tax;
+        }
+    }
+    return total;
 }
+
+
+
+
+
